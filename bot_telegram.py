7455a933d56eb4d6b241f479ac7edd54f60558bd
elchapas__ <99326658+wakicampos@users.noreply.github.com>
2024-01-15 15:42:46 +0100

    Update bot_telegram.py

--- bot_telegram.py
+++ bot_telegram.py
@@ -2,32 +2,31 @@ from functions import is_port_open, is_service_running, is_http_200_ok, send_tel
 import asyncio
 import os
 
-try:
-    async def monitor_website(url, interval, bot_token, chat_id):
-    while True:
-        if not await is_service_running(url):
-            await send_telegram_message(bot_token, chat_id, f"El servicio web en {url} no está funcionando.")
-            break
-        elif not await is_port_open(url, 80):
-            await send_telegram_message(bot_token, chat_id, f"El puerto 80 de {url} está cerrado o no responde.")
-            break
-        elif not await is_http_200_ok(url):
-            await send_telegram_message(bot_token, chat_id, f"El servicio web en {url} está devolviendo un código de estado HTTP que no es 200.")
-            break
-        await asyncio.sleep(interval)
-        
-    except asyncio.CancelledError:
-        send_telegram_message(bot_token, chat_id, f"La tarea ha sido cancelada.")
-        break
-        
-finally:
-    send_telegram_message(bot_token, chat_id, f"Cierre del bot...")
-    break
+async def monitor_website(url, interval, bot_token, chat_id):
+    try:
+        while True:
+            if not await is_service_running(url):
+                await send_telegram_message(bot_token, chat_id, f"El servicio web en {url} no está funcionando.")
+                break
+            elif not await is_port_open(url, 80):
+                await send_telegram_message(bot_token, chat_id, f"El puerto 80 de {url} está cerrado o no responde.")
+                break
+            elif not await is_http_200_ok(url):
+                await send_telegram_message(bot_token, chat_id, f"El servicio web en {url} está devolviendo un código de estado HTTP que no es 200.")
+                break
+            await asyncio.sleep(interval)
 
-        
+    except asyncio.CancelledError:
+        await send_telegram_message(bot_token, chat_id, f"La tarea ha sido cancelada.")
+        break
+    finally:
+        await send_telegram_message(bot_token, chat_id, f"Cierre del bot...")
+        break
+
 if __name__ == "__main__":
     url = "http://scanme.nmap.org"
-    interval = 10 # segundos
+    interval = 10  # segundos
     bot_token = os.getenv('BOT_TOKEN')
     chat_id = os.getenv('CHAT_ID')
     asyncio.run(monitor_website(url, interval, bot_token, chat_id))
+    
