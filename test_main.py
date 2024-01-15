6aaff01420374666f93614692e2e258d569c0140
elchapas__ <99326658+wakicampos@users.noreply.github.com>
2024-01-15 17:11:03 +0100

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,9 +2,7 @@ import sys
 sys.path.append('.')
 from functions import is_port_open, is_service_running, is_http_200_ok, send_telegram_message
 import pytest
-import asyncio
 
-async def test(bot_token, chat_id):
     @pytest.mark.asyncio
     async def test_is_port_open_success():
         print("Testing is_port_open with expected success...")
@@ -41,20 +39,3 @@ async def test(bot_token, chat_id):
         print("Testing is_http_200_ok with a non-200 status response...")
         assert await is_http_200_ok("http://example.com/notfoundpage") == False
         print("Non-200 status case passed.")
-
-    await send_telegram_message(bot_token, chat_id, f"Se puede hacer el PUSH ")
-        
-except asyncio.CancelledError:
-    await send_telegram_message(bot_token, chat_id, f"La tarea ha sido cancelada.")
-finally:
-    await send_telegram_message(bot_token, chat_id, f"Se cancela el PUSH")
-
-
-
-if __name__ == "__main__":
-    
-    bot_token = os.getenv('BOT_TOKEN')
-    chat_id = os.getenv('CHAT_ID')
-    asyncio.run(test(bot_token, chat_id))
-
-
